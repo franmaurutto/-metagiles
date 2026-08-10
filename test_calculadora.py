292e4f2d210c58eb89fb131835db91949aa40e6c
Francesca Maurutto <franchuls666@gmail.com>
2026-08-10 20:01:01 -0300

    cuarto test: red. implementacion de suma dos num enteros

--- test_calculadora.py
+++ test_calculadora.py
@@ -8,6 +8,8 @@ class TestCalculadora(unittest.TestCase):
         self.assertEqual(Sumar(""), 0)
     def test_un_numero_devuelve_el_mismo_numero(self):
         self.assertEqual(Sumar("1"), 1)
+    def test_dos_numeros_devuelve_la_suma(self):
+        self.assertEqual(Sumar("1,2"), 3)
 
 
 if __name__ == "__main__":
