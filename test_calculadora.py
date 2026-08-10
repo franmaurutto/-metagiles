7c237d6e535a98d1e64eb2656ab99a8ba66bb502
Francesca Maurutto <franchuls666@gmail.com>
2026-08-10 19:56:46 -0300

    tercer test: red. implementacion un numero y vacio

--- test_calculadora.py
+++ test_calculadora.py
@@ -6,6 +6,8 @@ class TestCalculadora(unittest.TestCase):
 
     def test_string_vacio_devuelve_cero(self):
         self.assertEqual(Sumar(""), 0)
+    def test_un_numero_devuelve_el_mismo_numero(self):
+        self.assertEqual(Sumar("1"), 1)
 
 
 if __name__ == "__main__":
