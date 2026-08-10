ec9d93c584123d123f536883b2da484a56b20518
Francesca Maurutto <franchuls666@gmail.com>
2026-08-10 20:02:38 -0300

    quinto test: green. implementacion de suma dos num enteros

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,9 @@
 def Sumar(numeros):
     if numeros == "":
         return 0
-    return int(numeros)
+    numeros = numeros.split(",")
+
+    if len(numeros) == 1:
+        return int(numeros[0])
+
+    return int(numeros[0]) + int(numeros[1])
